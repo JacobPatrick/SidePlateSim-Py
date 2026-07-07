67506f4ddd240bdd6e8e29da3f2f22a88e5d21d1
JacobPatrick <jacob_patrick@163.com>
2026-07-07 21:33:32 +0800

    (feat) add collision detection and handling to the dynamics solver

--- src/solver/forward_dynamics.py
+++ src/solver/forward_dynamics.py
@@ -4,8 +4,21 @@ from interface.type import (
     SidePlateMassProp,
     ForceTorque,
 )
-from utils.math_tools import quaternion_multiply
+from utils.math_tools import quaternion_multiply, quaternion_to_euler
 
+DRIVE_GEAR_CENTER = (0.0305, 0)
+SLAVE_GEAR_CENTER = (-0.0305, 0)
+GEAR_RADIUS = 0.035
+
+
+def _collision_detect(z, pitch, roll):
+    """碰撞判据: 齿顶圆上的点与侧板底部所在平面距离是否小于等于零"""
+    A = -GEAR_RADIUS * np.sin(pitch)
+    B = GEAR_RADIUS * np.sin(roll)
+    C1 = z - DRIVE_GEAR_CENTER[0] * np.sin(pitch)
+    C2 = z - SLAVE_GEAR_CENTER[0] * np.sin(pitch)
+
+    return (A**2 + B**2 >= C1**2) or (A**2 + B**2 >= C2**2)
 
 class ForwardDynamicsSolver:
     def __init__(self, physics_param: SidePlateMassProp):
@@ -96,5 +109,17 @@ class ForwardDynamicsSolver:
 
         q += q_dot * dt
         q /= np.linalg.norm(q)
+
+        # 5. 碰撞检测
+        z = p[2].copy()
+        roll, pitch, _ = quaternion_to_euler(q)
+        _entered_loop = False
+        while _collision_detect(z, pitch, roll):
+            _entered_loop = True
+            z += 1e-6  # 微调位置，避免碰撞
+        else:
+            if _entered_loop:
+                v = np.zeros(3)
+                w = np.zeros(3)
 
         return SidePlateState(p=p, v=v, q=q, w=w)
